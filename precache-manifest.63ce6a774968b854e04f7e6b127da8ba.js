2c3916c3b9a8791df1e4fd7e0af25f106b9bc154
adoankim <adoankim@gmail.com>
2020-06-22 00:10:44 +0100

    Base gh-pages

---
+++ precache-manifest.63ce6a774968b854e04f7e6b127da8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcee62b309cddc5f773c5c3793d69a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b2ad50b432e2f6d3be",
+    "url": "/static/css/main.d1a198a2.chunk.css"
+  },
+  {
+    "revision": "48c53083882845ffcad6",
+    "url": "/static/js/2.5b9f9448.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b9f9448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b2ad50b432e2f6d3be",
+    "url": "/static/js/main.8720fde0.chunk.js"
+  },
+  {
+    "revision": "775f15143caaf446324e",
+    "url": "/static/js/runtime-main.187327c3.js"
+  }
+]);
